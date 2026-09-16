a174a86178b00a22d52b6cd8c9c6997627f4323e
Nicanor Pujol <nicanorpujol1@gmail.com>
2026-09-15 23:38:38 -0300

    Agregar pruebas unitarias para la clase Pistola

---
+++ Test/Armas.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "vitest";
+import { Pistola } from "../Naval/Armas/Pistola.js";
+import { Ametralladora } from "../Naval/Armas/Ametralladora.js";
+
+describe("Pistola", () => {
+
+  test("debe comenzar con las municiones indicadas", () => {
+    const pistola = new Pistola(5);
+
+    expect(pistola.getMuniciones()).toBe(5);
+  });
+
+  test("debe descontar una municion al disparar", () => {
+    const pistola = new Pistola(5);
+
+    pistola.disparar();
+
+    expect(pistola.getMuniciones()).toBe(4);
+  });
+
+  test("debe hacer 1 de daño", () => {
+    const pistola = new Pistola(5);
+
+    expect(pistola.disparar()).toBe(1);
+  });
+
+  test("sin municiones no debe hacer daño", () => {
+    const pistola = new Pistola(0);
+
+    expect(pistola.disparar()).toBe(0);
+  });
+
+});
