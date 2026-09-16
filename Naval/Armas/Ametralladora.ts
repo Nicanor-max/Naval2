ba2b05940d2726d7bcd914da620b37899502f216
Nicanor Pujol <nicanorpujol1@gmail.com>
2026-09-15 23:35:43 -0300

    Implementar clase Ametralladora con métodos para disparar y obtener municiones

---
+++ Naval/Armas/Ametralladora.ts
@@ -0,0 +1,22 @@
+import type { Arma } from "./Armas.js";
+
+export class Ametralladora implements Arma {
+  private municiones: number;
+
+  constructor(municiones: number) {
+    this.municiones = municiones;
+  }
+
+  disparar(): number {
+    if (this.municiones <= 0) {
+      return 0;
+    }
+
+    this.municiones--;
+    return 2;
+  }
+
+  getMuniciones(): number {
+    return this.municiones;
+  }
+}
