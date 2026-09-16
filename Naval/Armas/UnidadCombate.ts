1d77bd12bcb0f4910b682dc40b5a614465bca505
Nicanor Pujol <nicanorpujol1@gmail.com>
2026-09-15 23:30:37 -0300

    Agregar métodos para disparar, recibir disparo, verificar estado y equipar escudo en UnidadCombate

--- Naval/Armas/UnidadCombate.ts
+++ Naval/Armas/UnidadCombate.ts
@@ -11,4 +11,26 @@ export abstract class UnidadCombate {
     this.arma = arma;
     this.escudo = new Escudo(0);
   }
+  disparar(objetivo: UnidadCombate): void {
+    const danio = this.arma.disparar();
+    objetivo.recibirDisparo(danio);
+  }
+
+  recibirDisparo(danio: number): void {
+    const danioFinal = this.escudo.reducirDanio(danio);
+
+    this.vida = Math.max(0, this.vida - danioFinal);
+  }
+
+  estaVivo(): boolean {
+    return this.vida > 0;
+  }
+
+  equiparEscudo(escudo: Escudo): void {
+    this.escudo = escudo;
+  }
+
+  getVida(): number {
+    return this.vida;
+  }
 }
